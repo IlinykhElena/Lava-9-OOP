a85b4b4134ce76797ee1e9fb830dfaa24d0aea41
ElenaIlinykh <bengaliki22@gmail.com>
2022-07-11 23:51:53 +0700

    FormDate type changed to int

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,9 +8,9 @@ public class Main {
         post.surname = "Иванов";
         post.subscription = true;
         post.birthday = new FormDate();
-        post.birthday.day = "13";
-        post.birthday.month = "06";
-        post.birthday.year = "1999";
+        post.birthday.day = 13;
+        post.birthday.month = 06;
+        post.birthday.year = 1999;
     }
 }
 
